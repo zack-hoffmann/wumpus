23b61fe95e8315d4e4ab765288f7e48dfe127213
Zack Hoffmann <zachary.hoffmann@gmail.com>
2019-12-31 18:31:03 -0500

    Making progress on session pool

--- src/main/java/wumpus/SessionPool.java
+++ src/main/java/wumpus/SessionPool.java
@@ -1,7 +1,9 @@
 package wumpus;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.eclipse.jetty.websocket.api.Session;
 
@@ -14,13 +16,44 @@ public interface SessionPool {
 
     static SessionPool recall(final Context ctx, final String name) {
         return SessionPoolHeap.INST.computeIfAbsent(
-                StringPool.tokenPool(ctx).intern(name),
-                n -> (() -> new HashMap<String, Session>()));
+                StringPool.tokenPool(ctx).intern(name), n -> HashMap::new);
+    }
+
+    static SessionPool initialPool(final Context ctx) {
+        return recall(ctx, ctx.requiredProperty("session.pool.initial.name"));
+    }
+
+    static SessionPool playerPool(final Context ctx) {
+        return recall(ctx, ctx.requiredProperty("session.pool.player.name"));
+    }
+
+    static SessionPool characterPool(final Context ctx) {
+        return recall(ctx, ctx.requiredProperty("session.pool.character.name"));
+    }
+
+    static void cleanAll() {
+        SessionPoolHeap.INST.values().stream().forEach(SessionPool::clean);
     }
 
     Map<String, Session> map();
 
+    default String register(final Session s, final Context ctx) {
+        final String token = StringPool.tokenPool(ctx).newToken();
+        map().put(token, s);
+        return token;
+    }
+
+    default String renew(final String token, final Context ctx) {
+        final Session s = map().get(token);
+        map().remove(token);
+        return register(s, ctx);
+    }
+
     default void clean() {
+        final List<String> expired = map().entrySet().stream()
+                .filter(es -> !es.getValue().isOpen()).map(es -> es.getKey())
+                .collect(Collectors.toList());
+        expired.stream().forEach(s -> map().remove(s));
 
     }
 }
